143b7bf18ee669e0cce5d7b65d798a2cb4128ae9
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-01-28 21:48:03 +0600

    splash screen compplete

--- lib/screen/onboarding/loginScreen.dart
+++ lib/screen/onboarding/loginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tm_app_rest_api/style/style.dart';
 
 class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
@@ -10,7 +12,29 @@ class loginScreen extends StatefulWidget {
 class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                    "Get Started with",
+                     style: Head1Text(colorDarkBlue),
+
+
+                )
+              ],
+
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
--- lib/screen/onboarding/splashScreen.dart
+++ lib/screen/onboarding/splashScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tm_app_rest_api/style/style.dart';
 
 class splashScreen extends StatefulWidget {
   const splashScreen({super.key});
@@ -10,6 +13,24 @@ class splashScreen extends StatefulWidget {
 class _splashScreenState extends State<splashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      //niche akta layer upore arekta layer design a background modhe akta design ase
+      body: Stack(
+        children: [
+          //first bg
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/images/logo.svg',
+                //assets/images/logo.svg
+                alignment: Alignment.center,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
