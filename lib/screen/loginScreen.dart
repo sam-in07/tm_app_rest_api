01f27bf4669eeda6b7b5189d69cb15e63e98923c
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-01-28 11:02:27 +0600

    screen update

---
+++ lib/screen/loginScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({super.key});
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
---
+++ lib/screen/splashScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({super.key});
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
