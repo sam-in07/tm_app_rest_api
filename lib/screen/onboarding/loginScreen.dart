a839e218ad964280dbb25d442cface2b35e84664
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-01-29 19:03:02 +0600

    apiClient login req update

--- lib/screen/onboarding/loginScreen.dart
+++ lib/screen/onboarding/loginScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tm_app_rest_api/api/apiClient.dart';
 import 'package:tm_app_rest_api/style/style.dart';
 
 class loginScreen extends StatefulWidget {
@@ -10,6 +11,48 @@ class loginScreen extends StatefulWidget {
 }
 
 class _loginScreenState extends State<loginScreen> {
+
+  Map<String,String> FormValues={"email":"", "password":""};
+  bool Loading = false ;
+
+  InputOnChnage(Mapkey,Textvalue){
+    setState(() {
+      FormValues.update(Mapkey, (value) => Textvalue);
+    });
+  }
+
+ FormOnSubmit() async{
+   if(FormValues['email']!.length==0) {
+     ErrorToast("Email is required");
+
+   }
+   else if (FormValues['password']!.length==0) {
+     ErrorToast("password is required");
+
+   }
+   else {
+     setState(() {
+       Loading = true;
+     });
+     bool Result = await LoginRequest(FormValues);
+
+     if(Result ==  true){
+       //navigate to dashboard page
+     }
+     if(Result ==  false){
+       //navigate to dashboard page
+       setState(() {
+         Loading = false;
+       });
+     }
+
+     setState(() {
+       Loading = false;
+     });
+   }
+ }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,29 +66,47 @@ class _loginScreenState extends State<loginScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                    "Set Password",
-                     style: Head1Text(colorDarkBlue),
+                  "Get Started with",
+                  style: Head1Text(colorDarkBlue),
                 ),
                 SizedBox(height: 1,),
                 Text(
-                  "Minimum length password 8 character with Latter and number combination ",
+                  "Learn with Rabbil hasan",
                   style: Head2Text(colorLightGray),
                 ),
                 SizedBox(height: 20,),
+
                 TextFormField(
+                  onChanged: (Textvalue){
+                   InputOnChnage("email",Textvalue);
+                  },
+
+                  decoration: AppInputDecoration("Email Address"),
+
+                ),
+
+                SizedBox(height: 20,),
+
+                TextFormField(
+                  onChanged: (Textvalue){
+                    InputOnChnage("password",Textvalue);
+                  },
+
+
                   decoration: AppInputDecoration("Password"),
+
                 ),
+
+
                 SizedBox(height: 20,),
-                TextFormField(
-                  decoration: AppInputDecoration("Confirm Password"),
-                ),
-                SizedBox(height: 20,),
+
+
                 Container(
                   child: ElevatedButton(
                     style: AppButtonStyle(),
                     child: SuccessButtonChild('Confirm'),
                     onPressed: () {
-
+                       FormOnSubmit();
                     },
                   ),
                 )
