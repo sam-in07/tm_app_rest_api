636b27ee814d3daf778ded0c2fa8ac0fa458083f
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-01-29 12:12:14 +0600

    pin verificaton complete

--- lib/screen/onboarding/pinVerificationScreen.dart
+++ lib/screen/onboarding/pinVerificationScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:tm_app_rest_api/style/style.dart';
 
 class pinVerificationScreen extends StatefulWidget {
   const pinVerificationScreen({super.key});
@@ -10,6 +13,50 @@ class pinVerificationScreen extends StatefulWidget {
 class _pinVerificationScreenState extends State<pinVerificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body : Stack(
+        children:[
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children : [
+                Text(
+                  "Pin Verification",
+                  style: Head1Text(colorDarkBlue),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "A 6 digit verification pin will send to your mobile number",
+                  style: Head2Text(colorLightGray),
+
+                ),
+                SizedBox(height: 20),
+                PinCodeTextField(
+                    appContext: context,
+                    length: 6,
+                    pinTheme: AppOTPStyle(),
+                   animationType: AnimationType.fade,
+                   animationDuration: Duration(milliseconds: 300),
+                   enableActiveFill: true,
+                   onCompleted: (v){
+
+                   },
+                  onChanged: (value){
+
+                  },
+
+
+
+                )
+
+              ]
+            )
+          )
+        ]
+      )
+    );
   }
 }
