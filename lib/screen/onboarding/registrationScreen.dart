82ad70c34b75bafa6acfe3536f6e82c90fd528ea
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2026-01-29 08:14:50 +0600

    registration complete

--- lib/screen/onboarding/registrationScreen.dart
+++ lib/screen/onboarding/registrationScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tm_app_rest_api/style/style.dart';
 
 class registrationScreen extends StatefulWidget {
   const registrationScreen({super.key});
@@ -10,6 +12,62 @@ class registrationScreen extends StatefulWidget {
 class _registrationScreenState extends State<registrationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Join With US",
+                  style: Head1Text(colorDarkBlue),
+                ),
+                SizedBox(height: 1,),
+                Text(
+                  "Learn with Rabbil hasan",
+                  style: Head2Text(colorLightGray),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: AppInputDecoration("Email Address"),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: AppInputDecoration("First Name"),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: AppInputDecoration("Last Name"),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: AppInputDecoration("Mobile"),
+                ),
+                SizedBox(height: 20,),
+                TextFormField(
+                  decoration: AppInputDecoration("Confirm  Password"),
+                ),
+
+                SizedBox(height: 20,),
+                Container(
+                  child: ElevatedButton(
+                    style: AppButtonStyle(),
+                    child: SuccessButtonChild('Registration'),
+                    onPressed: () {
+
+                    },
+                  ),
+                )
+              ],
+
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
